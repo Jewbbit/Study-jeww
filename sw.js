9ad68ad4aa7f71c2240cee4c88d0a7d0c3d9af0a
Jewbbit <rbyaze@naver.com>
2026-08-15 19:28:06 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME="study-jew-pwa-v1";
+const CORE=["./edit.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  const req=event.request;
+  if(req.mode==="navigate"){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE_NAME).then(cache=>cache.put("./edit.html",copy));
+        return res;
+      }).catch(()=>caches.match("./edit.html"))
+    );
+    return;
+  }
+  const url=new URL(req.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(
+      caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE_NAME).then(cache=>cache.put(req,copy));
+        return res;
+      }))
+    );
+  }
+});
